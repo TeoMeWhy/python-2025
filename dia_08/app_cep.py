82977a23e8239620dfb820fa68f187e7d3303e71
Téo Calvo <teocalvo2@gmail.com>
2025-02-19 12:44:38 -0300

    cep

--- dia_08/app_cep.py
+++ dia_08/app_cep.py
@@ -6,11 +6,9 @@ import requests
 URL = "https://viacep.com.br/ws/{cep}/json/"
 
 st.title("Busca CEP")
-
 cep = st.text_input("Busque seu cep")
 
 if cep != "":
-    
     try:
         resp = requests.get(URL.format(cep=cep))
         data = pd.DataFrame([resp.json()])
