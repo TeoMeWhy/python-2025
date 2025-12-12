a583b879b92bfb54a2bb2e9fd2ac2d3c31e8398d
Téo Calvo <teocalvo2@gmail.com>
2025-12-12 10:47:03 -0300

    feat: exercícios de laço de repetição

---
+++ exercicios_aula/03_01_03.py
@@ -0,0 +1,8 @@
+# Faça um programa que conte quantas vezes a letra “a” aparece em uma palavra
+
+palavra = input("Entre com uma palavra: ").lower()
+
+# Usando método das strings
+count = palavra.count("a")
+
+print(f"A palavra '{palavra}' tem {count} 'a'.")
